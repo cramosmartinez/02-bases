c0f5122ea19af6dfde3d88892893bf35b6df6e4d
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-03 19:20:11 -0600

    Creacion de callbacks con funciones llamando los id en la consola

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-const { emailTemplate } = require('./js-foundation/01-template.js' );
-require('./js-foundation/02-destructuring.js' );
+//const { emailTemplate } = require('./js-foundation/01-template.js' );
+//require('./js-foundation/02-destructuring.js' );
+const { getUserById } = require("./js-foundation/03-callbacks.js");
 
-
-
-
+const id = 1;
+getUserById(id, function (error, user) {
+  if (error) {
+    throw new Error(error);
+  }
+  console.log(user);
+});
 
 //console.log(emailTemplate);
 //console.log(templateExports.emailTemplate);
