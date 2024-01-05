c3118d3e978a308ac637cca363971d75a1a402ac
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-04 20:02:28 -0600

    Creacion de promesas con api buscar un pokemon por id

--- src/app.js
+++ src/app.js
@@ -3,9 +3,24 @@
 //const { getUserById } = require("./js-foundation/03-callbacks.js");
 //const { getUserById } = require("./js-foundation/04-arrow.js");
 //const {} = require ('./js-foundation/05-factory.js')
-const {buildMakePerson} = require('./js-foundation/05-factory.js');
+const getPokemonById = require('./js-foundation/06-promises.js');
+
+getPokemonById(5, (pokemon) => {
+    console.log({pokemon});
+});
+
+
+
+
+
+
+
+
+
+//referiencia de la funcion factory
+// const {buildMakePerson} = require('./js-foundation/05-factory.js');
 const { getUUID, getAge } = require('./plugins');
-const makePerson = buildMakePerson({ getUUID, getAge });
-const obj = { name: "John", birthdate: "1985/10/21" }; // Object literal 
-const John = makePerson(obj);
-console.log(John);
+// const makePerson = buildMakePerson({ getUUID, getAge });
+// const obj = { name: "John", birthdate: "1985/10/21" }; // Object literal 
+// const John = makePerson(obj);
+// console.log(John);
