2d9b483b562a3bbbb1e6033f8d5c08df89dd5361
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-04 20:26:39 -0600

    Patrón adaptador - FetchAPI

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,9 @@
 const {getUUID} = require("../plugins/get-id.plugin.js");
 const {getAge} = require("../plugins/get-age.plugin.js");
+const { http } = require("../plugins/http-client.plugin.js");
 
 module.exports = {
     getUUID,
     getAge,
+    http 
 }
