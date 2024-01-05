09bdf7a7350e5c9ae1f6383137c3c5cfca05eb39
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-04 20:16:05 -0600

    Async - Await aplicado en 06-promises.js

--- src/js-foundation/06-promises.js
+++ src/js-foundation/06-promises.js
@@ -1,9 +1,13 @@
-const getPokemonById = (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  return fetch(url)
-    .then((resp) => resp.json())
-    //.then(() => { throw new Error('Pokemon no existe')})
-    .then((pokemon) => pokemon.name);
+const getPokemonById = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const resp = await fetch(url);
+    const pokemon = await resp.json();
+    return pokemon.name;
+//   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+//   return fetch(url)
+//     .then((resp) => resp.json())
+//     //.then(() => { throw new Error('Pokemon no existe')})
+//     .then((pokemon) => pokemon.name);
 };
 
 module.exports = getPokemonById;
